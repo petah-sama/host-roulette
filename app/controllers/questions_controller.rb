0d85b03d9be2308aaed7a46c8df9518f893778de
Pedro Batista <pedrobatista.420@gmail.com>
2020-03-02 18:12:29 +0000

    controllers, hopefuly done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,4 @@
+class QuestionsController < ApplicationController
+  def create
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,7 @@
+class ReviewsController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+end
