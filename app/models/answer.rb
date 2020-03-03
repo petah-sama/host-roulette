be91299a5e122192ff335c7336906d67f1ed1043
Pedro Batista <pedrobatista.420@gmail.com>
2020-03-03 13:13:14 +0000

    models, colors and fonts :)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :guest
+
+  validates :content, presence: true
 end
--- app/models/edition.rb
+++ app/models/edition.rb
@@ -2,4 +2,7 @@ class Edition < ApplicationRecord
   enum status: %i[pending active archived]
 
   belongs_to :event
+  has_many :guests
+  has_many :questions
+  has_many :items
 end
