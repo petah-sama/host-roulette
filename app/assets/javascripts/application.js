d64584e1dc58164295168469ab5814b223a93a0c
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-04 15:29:52 +0000

    changed js bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,4 @@
-var filterHost = document.getElementById('filter-by-host');
-
-var list = document.querySelectorAll("active-card");
-var filterActive = document.getElementById('filter-by-state');
-
-
- function hidingPending () {
-  var x = document.getElementById("active-cards");
-  if (x.style.display === "none") {
-    filterActive.innerHTML = "Active";
-    x.style.display = "block";
-  } else {
-    filterActive.innerHTML = "Pending";
-    x.style.display = "none";
-  };
-};
+//= require rails-ujs
+//= require_tree .
+//= require owl.carousel
 
