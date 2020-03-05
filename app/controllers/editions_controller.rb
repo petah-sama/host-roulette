5a0493929fb3325cf9f670be81e9d0676e13b324
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-05 18:11:25 +0000

    host-permissions

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -5,7 +5,6 @@ class EditionsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
@@ -16,12 +15,20 @@ class EditionsController < ApplicationController
   end
 
   def create
+    fetch_event
+    @edition = Edition.new(edition_params)
+    @edition.event = @event
+      @edition.event.members.each do |member|
+        @edition.guests.each do |guest|
+          guest = Guest.new
+          guest.user = guest.member.user
+        end
+      end
     authorize @edition
   end
 
   def edit
     events = policy_scope(Event).includes(:members).where(members: { user:current_user })
-
   end
 
   def update
@@ -47,8 +54,6 @@ class EditionsController < ApplicationController
     @edition = Edition.find(params[:id])
     authorize @edition
   end
-
-
 
 
   def edition_params
