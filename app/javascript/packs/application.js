3bd1abb30315b02e19332285665b639a4dc9d367
Pedro Batista <pedrobatista.420@gmail.com>
2020-03-09 11:40:54 +0000

    refresh invites, roulette size and spining

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import { sliderBudgetValue } from '../components/event_form';
 
 import { editionRest } from '../components/roulette';
 
+import { refreshMembers } from '../components/invite_members_refresh.js';
+
 // // NEW EVENT FORM
 const tagForm = document.querySelector("#new_event");
 if (tagForm) {
@@ -20,3 +22,9 @@ const roulettePage = document.querySelector('#wheel');
 if (roulettePage) {
   editionRest();
 };
+
+// Refresh invitiation page every 5 seconds
+const invitePage = document.querySelector('#invite-members');
+if (invitePage) {
+  refreshMembers();
+}
