5e5b2d549ab1cc9305f65883c50e78f20c14918b
Margarida Toureiro <margaridatoureiro@gmail.com>
2020-03-10 17:33:37 +0000

    step by step

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -15,7 +15,7 @@ class NotificationsController < ApplicationController
     @notifications.each do |notification|
       notification.destroy
     end
-    redirect_to dashboard_path
+    redirect_back(fallback_location: root_path)
   end
 
 
