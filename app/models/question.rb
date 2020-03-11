43adceb43c5947dc17a7763d3ea9c88a692ee75c
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-11 11:21:39 +0000

    notification-reminder

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :edition
   has_many :answers, dependent: :destroy
+  has_many :guests, through: :answers
 
   validates :content, presence: true
+
+
 end
