870e4d16d8878600ada02aae7e1abb947602c4c8
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-11 17:23:04 +0000

    unfinished charts for surveys

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,10 @@ class QuestionsController < ApplicationController
   before_action :fetch_event_edition, except: :destroy
   before_action :fetch_question, only: %i[show destroy]
 
+  def index
+    @questions = policy_scope(Question).where(edition: @edition)
+  end
+
   def create
     @question = Question.new(question_params)
     @question.edition = @edition
@@ -20,26 +24,13 @@ class QuestionsController < ApplicationController
 
   def destroy
     if @question.destroy
-      redirect_to event_edition_path(@question.edition.event, @question.edition)
+      redirect_to event_edition_questions_path(@question.edition.event, @question.edition)
     else
       render 'questions/show'
     end
   end
 
   private
-
-
-
-  def send_reminders
-    current_guest = current_user.guest_for(@question.edition)
-    @question.guests_didnt_answer.each do |guest|
-      @notification = Notification.new
-      @notification.edition = @question.edition
-      @notification.from = 'reminder'
-      @notification.user = guest.member.user
-      @notification.save
-    end
-  end
 
 
   def status_notification(from)
