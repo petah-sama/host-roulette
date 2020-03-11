8738effcac883fc9b87445bb63b9425c2add9b1c
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-11 20:48:30 +0000

    surveys-index

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,6 @@ class Answer < ApplicationRecord
   belongs_to :guest
 
   validates_uniqueness_of :guest_id, scope: :question_id
+
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,45 @@ class Question < ApplicationRecord
   validates :content, presence: true
 
 
+
+
+  def answered_yes
+    answered_yes = []
+    self.answers.each do |answer|
+      answered_yes << answer if answer.content == true
+    end
+    answered_yes.size
+  end
+
+  def answered_no
+    answered_no = []
+    self.answers.each do |answer|
+      answered_no << answer if answer.content == false
+    end
+    answered_no.size
+  end
+  def unanswered
+    unanswered = (self.edition.guests.size - 1)-(self.answered_yes + self.answered_no)
+  end
+
+  def unanswered_questions
+    unanswered = []
+    self.answers.each do |answer|
+      unanswered << answer if answer.content == false
+    end
+  end
+
+  def guests_didnt_answer
+    guests = []
+     self.edition.guests.each do |guest|
+      guest.answers.each do |answer|
+        if answer.question == self && self.edition.host != guest.member.user
+          guests << guest
+        end
+      end
+    end
+    return guests
+  end
+
+
 end
